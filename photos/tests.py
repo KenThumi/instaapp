6c339808c35afa1d90c70307bc4858a369de614d
Ken Thumi <kenthumi@gmail.com>
2021-05-23 03:38:42 +0300

    Image save_image method test pass

--- photos/tests.py
+++ photos/tests.py
@@ -1,4 +1,4 @@
-from photos.models import Profile
+from photos.models import Image, Profile
 from django.test import TestCase
 from django.contrib.auth.models import User
 
@@ -21,3 +21,29 @@ class ProfileTestClass(TestCase):
         profiles = Profile.objects.all()
 
         self.assertTrue(len(profiles)>0)
+
+
+
+class ImageTestClass(TestCase):
+    '''Test methods of Image model'''
+
+    def setUp(self):
+        self.user_1 = User.objects.create_user('Chevy Chase', 'chevy@chase.com', 'chevyspassword')
+        self.profile=Profile(profile_photo='imageurl.png',bio='Lorem ipsum',user=self.user_1)
+        self.image=Image(image='newimageurl.png',name='Test Image',caption='Lorem ipsum',profile=self.profile)
+
+    
+    def tearDown(self):
+        self.image.delete()
+        self.profile.delete()
+        self.user_1.delete()
+
+
+    def test_save_image(self):
+        self.user_1.save()
+        self.profile.save()
+        self.image.save()
+
+        images = Image.objects.all()
+
+        self.assertTrue(len(images)>0)
